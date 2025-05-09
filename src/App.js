058e2864fc48af80655f48d29343db009b0582c6
Naeim <naeim.smz@gmail.com>
2025-05-09 19:26:52 +0430

    cleaning up data fetching

--- src/App.js
+++ src/App.js
@@ -79,13 +79,16 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -99,7 +102,10 @@ export default function App() {
         } catch (err) {
           if (err.name !== "AbortError") {
             console.log(err.message);
-            setError(err.message);
+
+            if (err.name !== "AbortError") {
+              setError(err.message);
+            }
           }
         } finally {
           setIsLoading(false);
@@ -113,6 +119,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
